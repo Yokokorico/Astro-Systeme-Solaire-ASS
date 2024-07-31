b55208ecdc4c0065b8883576f8426d5a11f72671
Vivien Gajac <viviengajac@gmail.com>
2024-07-31 16:14:17 +0200

    🚧 init self dev branch

--- src/app/components/ThreeScene.tsx
+++ src/app/components/ThreeScene.tsx
@@ -8,12 +8,12 @@ const ThreeScene = () => {
     const [hovered, setHovered]= useState(false);
  return(
     <div id="canvas-container">
-        <Canvas style={{height: '100vh', width: '100vw'}}>
+        <Canvas style={{height: '100vh', width: '100vw', background: 'black'}}>
             <ambientLight intensity={1} />
             <directionalLight color="white" position={[0, 0, 5]} />
-            <Planet scale={[0.0071492,0.0071492, 0.0071492]} name="jupiter" position={[500, 0, 0]}/>
-            <Planet scale={[0.0006378,0.0006378 ,0.0006378]} name="terre" position={[-500, 0 ,0 ]} />
-            <Planet scale={[0.0696342,0.0696342, 0.0696342]} name="sun" />
+            <Planet scale={[0.0071492,0.0071492, 0.0071492]} name="jupiter" position={[250, 0, -100]}/>
+            <Planet scale={[0.0006378,0.0006378 ,0.0006378]} name="terre" position={[150, 0, -100]} />
+            <Planet scale={[0.0696342,0.0696342, 0.0696342]} name="sun" position={[0, 0, -100]}/>
             <OrbitControls />
         </Canvas>
     </div>

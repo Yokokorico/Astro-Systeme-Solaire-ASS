0bb1f789cf4414b9a5308e01e917374ee329a66f
Simon VINCENT <simon.dev.vincent@gmail.com>
2024-08-01 16:58:03 +0200

    add : astre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
+'use client';
 
+import React, { useState } from 'react';
 import AstreDetails from './components/AstreDetails';
-import ThreeScene from './components/ThreeScene'
+import ThreeScene from './components/ThreeScene';
 import AstroNav from './components/AstroNav';
 
-import "./variables.css";
+import './variables.css';
 
 function Home() {
-  
   const astroPlanets = ["soleil", "mercure", "venus", "terre", "mars", "jupiter", "saturne", "neptune", "uranus"];
+
+  const [selectedPlanet, setSelectedPlanet] = useState(astroPlanets[0]);
+
+  const handlePlanetChange = (newPlanet: string) => {
+    setSelectedPlanet(newPlanet);
+  };
 
   return (
     <div>
       <ThreeScene />
-        <AstroNav planets={astroPlanets}/>
-      </div>
-  )
+      <AstreDetails id={selectedPlanet} />
+      <AstroNav planets={astroPlanets} onPlanetChange={handlePlanetChange} />
+    </div>
+  );
 }
 
 export default Home;
-

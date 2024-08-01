b583baeb7bafa735c4d46b7af95789894da32732
Vivien Gajac <viviengajac@gmail.com>
2024-08-01 12:17:28 +0200

    🚧 add astrobar previous / next functions

--- src/app/components/AstroNav.tsx
+++ src/app/components/AstroNav.tsx
@@ -4,29 +4,38 @@ import { useState } from "react";
 import styles from '@/app/components/AstroNav.module.css';
 
 const AstroNav = () => {
-    const [hovered, setHovered]= useState(false);
+
+    const [isAtMin, setAtMin] = useState(true);
+    const [isAtMax, setAtMax] = useState(false);
+    const [indexObjects, setIndexObjects] = useState(0);
+
+    const countObjects = 3; // nombre d'objets visitables, défini par le tableau des objets
 
     function previous() {
-        console.log('previous');
+        setIndexObjects(indexObjects - 1);
+        indexObjects === countObjects ? setAtMax(false) : '';
+        indexObjects === 1 ? setAtMin(true) : '';
     }
 
     function next() {
-        console.log('next');
+        indexObjects === 0 ? setAtMin(false) : '';
+        setIndexObjects(indexObjects + 1);
+        indexObjects === countObjects - 1 ? setAtMax(true) : '';
     }
 
- return(
-    <div className="flex justify-center items-center" id={styles.astroNav}>
-            <button className="flex justify-center items-center" id={styles.previous} onClick={previous}>
-                <div className={styles.chevronDot}></div>
-            </button>
+    return(
+        <div className="flex justify-center items-center" id={styles.astroNav}>
+                <button className={`flex justify-center items-center ${isAtMin ? 'locked' : ''}`} id={styles.previous} onClick={previous}>
+                    <div className={styles.chevronDot}></div>
+                </button>
 
-        <p className="flex justify-center items-center" id={styles.current}>Soleil</p>
+            <p className="flex justify-center items-center" id={styles.current}>Système solaire {indexObjects}</p>
 
-            <button className="flex justify-center items-center" id={styles.next} onClick={next}>
-                <div className={styles.chevronDot}></div>
-            </button>
-    </div>
- )
+                <button className={`flex justify-center items-center ${isAtMax ? 'locked' : ''}`} id={styles.next} onClick={next}>
+                    <div className={styles.chevronDot}></div>
+                </button>
+        </div>
+    )
 };
 
 export default AstroNav;

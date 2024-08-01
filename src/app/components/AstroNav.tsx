a9806ba04fc4ea3cd2349a1e549d6cb19c3e58fe
Simon VINCENT <simon.dev.vincent@gmail.com>
2024-08-01 16:57:52 +0200

    add: appeler par astre

--- src/app/components/AstroNav.tsx
+++ src/app/components/AstroNav.tsx
@@ -3,11 +3,12 @@
 import { useState } from "react";
 import styles from '@/app/components/AstroNav.module.css';
 
-interface astroProps {
+interface AstroNavProps {
     planets: string[],
+    onPlanetChange: (planet: string) => void;
 }
 
-const AstroNav: React.FC<astroProps> = ({ planets }) =>  {
+const AstroNav: React.FC<AstroNavProps> = ({ planets, onPlanetChange }) => {
 
     const [isAtMin, setAtMin] = useState(true);
     const [isAtMax, setAtMax] = useState(false);
@@ -16,15 +17,21 @@ const AstroNav: React.FC<astroProps> = ({ planets }) =>  {
     const countObjects = planets.length; // nombre d'objets visitables, défini par le tableau des objets
 
     function previous() {
-        indexObjects === countObjects - 1 ? setAtMax(false) : '';
-        setIndexObjects(indexObjects - 1);
-        indexObjects === 1 ? setAtMin(true) : '';
+        if (indexObjects > 0) {
+            setIndexObjects(indexObjects - 1);
+            onPlanetChange(planets[indexObjects - 1]);
+            setAtMax(false);
+            if (indexObjects - 1 === 0) setAtMin(true);
+        }
     }
 
     function next() {
-        indexObjects === 0 ? setAtMin(false) : '';
-        setIndexObjects(indexObjects + 1);
-        indexObjects === countObjects - 2 ? setAtMax(true) : '';
+        if (indexObjects < countObjects - 1) {
+            setIndexObjects(indexObjects + 1);
+            onPlanetChange(planets[indexObjects + 1]);
+            setAtMin(false);
+            if (indexObjects + 1 === countObjects - 1) setAtMax(true);
+        }
     }
 
     return(
@@ -33,13 +40,15 @@ const AstroNav: React.FC<astroProps> = ({ planets }) =>  {
                 <div className={styles.chevronDot}></div>
             </button>
 
-            <p className="flex justify-center items-center" id={styles.current}>{planets[indexObjects].charAt(0).toUpperCase() + planets[indexObjects].slice(1)}</p>
+            <p className="flex justify-center items-center" id={styles.current}>
+                {planets[indexObjects].charAt(0).toUpperCase() + planets[indexObjects].slice(1)}
+            </p>
 
             <button className={`flex justify-center items-center ${isAtMax ? 'locked' : ''}`} id={styles.next} onClick={next}>
                 <div className={styles.chevronDot}></div>
             </button>
         </div>
-    )
+    );
 };
 
 export default AstroNav;

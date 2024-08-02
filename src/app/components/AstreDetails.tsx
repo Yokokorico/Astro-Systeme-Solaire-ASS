ab24c015395784c921bae49eff03ff37eaa0822a
Théo Duflos <152173306+Yokokorico@users.noreply.github.com>
2024-08-02 15:55:27 +0200

    fix: :bug: update import bug

--- src/app/components/AstreDetails.tsx
+++ src/app/components/AstreDetails.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState, useRef } from 'react';
-import ApiService from '../services/apiService';
+import ApiService from '../services/ApiService';
 import { Astre } from '../types/bodies';
 import { kelvinToCelsius } from '../utils/Conversion';
 import styles from './AstreDetails.module.css';

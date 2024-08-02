2c278e01ab3ebe26d7906ea13ed5461a9c34a135
Merlinovitch <a.merlin92@gmail.com>
2024-08-02 09:07:58 +0200

    feat: :sparkles: add orbitSpeed

--- src/app/components/AstreDetails.tsx
+++ src/app/components/AstreDetails.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import ApiService from '../services/apiService';
+import ApiService from '../services/ApiService';
 import { Astre }from '../types/bodies';
 import { kelvinToCelsius } from '../utils/Conversion';
 
--- src/app/components/ThreeScene.tsx
+++ src/app/components/ThreeScene.tsx
@@ -1,23 +1,25 @@
 'use client';
-import { Box, OrbitControls, Sphere } from "@react-three/drei";
+import * as THREE from 'three';  // Pour accéder au namespace THREE
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { Planet } from "./Planet";
 import Stars from "./Stars";
-import { angularSpeed, scaleOrbit, scaleRadius } from "../utils/Conversion";
-import getPlanetById from "../services/apiService";
+import { angularSpeed, calculateOrbitalSpeed, scaleOrbit, scaleRadius } from "../utils/Conversion";
+import getPlanetById from "../services/ApiService";
 import { Astre } from "../types/bodies";
-import { Sun } from "./Sun"
 import { Vector3 } from "three";
+import React from 'react';
+import { OrbitLine } from './OrbitLine';
 
 const ThreeScene = () => {
-    const [hovered, setHovered]= useState(false);
     const [data, setData] = useState<Astre[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
-    const sunRef : any = useRef();
-    const directionalLightRef = useRef<any>(); 
-    const planets = ["soleil", "mercure", "venus", "terre", "mars", "jupiter", "saturne", "neptune", "uranus" ]
+    const sunRef = useRef<THREE.Group>(null);
+    const directionalLightRef = useRef<THREE.PointLight>(null);
+    const planets = ["soleil", "mercure", "venus", "terre", "mars", "jupiter", "saturne", "neptune", "uranus"];
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -33,73 +35,79 @@ const ThreeScene = () => {
     }, []);
 
     useEffect(() => {
-            if (sunRef.current) {
-                const sunPosition = sunRef.current.position;
-                directionalLightRef.current.position.set(sunPosition.x, sunPosition.y, sunPosition.z);
-            }
-        }, [sunRef.current?.position]);
-        
-    
-    if(loading){
-        return;
+        if (sunRef.current && directionalLightRef.current) {
+            const sunPosition = sunRef.current.position;
+            directionalLightRef.current.position.set(sunPosition.x, sunPosition.y, sunPosition.z);
+        }
+    }, [sunRef.current?.position]);
+
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
-    if(error){
-        return <div>ERREUR</div>
+    if (error) {
+        return <div>Error: {error.message}</div>;
     }
 
     const findRadiusPlanetByName = (name: string): Vector3 => {
-        const planet = data.find(planet => 
-            planet.id?.toLowerCase() === name.toLowerCase()
-        );        
-        return new Vector3(scaleRadius(planet?.equaRadius),scaleRadius(planet?.equaRadius),scaleRadius(planet?.equaRadius))
+        const planet = data.find(planet => planet.id?.toLowerCase() === name.toLowerCase());
+        return new Vector3(scaleRadius(planet?.equaRadius), scaleRadius(planet?.equaRadius), scaleRadius(planet?.equaRadius));
     };
 
     const findPositionPlanetByName = (name: string): Vector3 => {
-        const planet = data.find(planet => 
-            planet.id?.toLowerCase() === name.toLowerCase()
-        );        
-        return new Vector3(scaleOrbit(planet?.semimajorAxis),0,0);
-    }
+        const planet = data.find(planet => planet.id?.toLowerCase() === name.toLowerCase());
+        return new Vector3(scaleOrbit(planet?.semimajorAxis), 0, 0);
+    };
 
-    const getAngularSpeed= (name: string): number => {
-        const planet = data.find(planet => 
-            planet.id?.toLowerCase() === name.toLowerCase()
-        );
-        return angularSpeed(planet?.sideralRotation)
-    }
+    const getAngularSpeed = (name: string): number => {
+        const planet = data.find(planet => planet.id?.toLowerCase() === name.toLowerCase());
+        return angularSpeed(planet?.sideralRotation);
+    };
+    const getOrbitalSpeed = (name: string): number => {
+        const planet = data.find(planet => planet.id?.toLowerCase() === name.toLowerCase());
+        // Utilisez la période orbitale (sideralOrbit) en jours pour calculer la vitesse angulaire
+        return calculateOrbitalSpeed(planet?.sideralOrbit);
+    };
+    
 
-    if(!loading && data){
-      
-        return(
-            <div id="canvas-container">
-                <div id="canvas-container">
-                <Canvas style={{ height: '100vh', width: '100vw' }} gl={{ antialias: true}} shadows>
-                    <ambientLight intensity={0.3}></ambientLight>
-                    <pointLight position={[0,1,0]} distance={0} power={550000} />
-                    <Planet 
-                        scale={[0.00696342, 0.00696342, 0.00696342]} 
-                        name="soleil" 
-                        angularSpeed={0}
-                        ref={sunRef}
-                    > 
-                    </Planet>
-                    {planets.filter(planet => planet !== 'soleil').map((planet) => (
-                        <Planet
-                            key={planet}
-                            scale={findRadiusPlanetByName(planet)}
-                            name={planet}
-                            position={findPositionPlanetByName(planet)}
-                            angularSpeed={getAngularSpeed(planet)}
-                        />
-                    ))}
-                    <OrbitControls />
-                    <Stars />
-                </Canvas>
-            </div>
-            </div>
-         )
-    }
+    return (
+        <div id="canvas-container">
+            <Canvas style={{ height: '100vh', width: '100vw' }} gl={{ antialias: true }} shadows>
+                <ambientLight intensity={0.3} />
+                <pointLight ref={directionalLightRef} position={[0, 1, 0]} distance={0} power={550000} />
+                <Planet
+                    scale={[0.00696342, 0.00696342, 0.00696342]}
+                    name="soleil"
+                    angularSpeed={0}
+                    semiMajorAxis={0}
+                    sideralOrbit={0}
+                    orbitCenter={new Vector3(0, 0, 0)}
+                    ref={sunRef}
+                />
+                {planets.filter(planet => planet !== 'soleil').map((planet) => (
+                    <React.Fragment key={planet}>
+                    <Planet
+                        key={planet}
+                        scale={findRadiusPlanetByName(planet)}
+                        name={planet}
+                        position={findPositionPlanetByName(planet)}
+                        angularSpeed={getAngularSpeed(planet)} // Peut ne pas être utilisé si vous calculez la vitesse orbitale
+                        sideralOrbit={getOrbitalSpeed(planet)}
+                        semiMajorAxis={scaleOrbit(data.find(p => p.id?.toLowerCase() === planet.toLowerCase())?.semimajorAxis || 1)}
+                        orbitCenter={new Vector3(0, 0, 0)} // Le centre de l'orbite est le soleil
+                    />
+                    <OrbitLine
+                    semiMajorAxis={scaleOrbit(data.find(p => p.id?.toLowerCase() === planet.toLowerCase())?.semimajorAxis || 1)}
+                    orbitCenter={new Vector3(0, 0, 0)}
+                    lineOpacity={0.1}
+                    />
+            </React.Fragment>
+                ))}
+                <OrbitControls />
+                <Stars />
+            </Canvas>
+        </div>
+    );
 };
 
 export default ThreeScene;

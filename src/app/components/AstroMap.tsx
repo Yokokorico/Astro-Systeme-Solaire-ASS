34a7df65dd5633ab26223fd7129a5fe1660022d5
Merlinovitch <a.merlin92@gmail.com>
2024-08-07 13:42:58 +0200

    feat: :construction: WIP create single api call

--- src/app/components/AstroMap.tsx
+++ src/app/components/AstroMap.tsx
@@ -12,40 +12,17 @@ import AstroPlanet from './AstroPlanet';
 import Stars from './Stars';
 
 export interface AstroMapProps {
-    astroType: AstroType[];
+    planets: Astre[],
     selectedPlanetId: string;
     speedRatio: number;
 }
 
-function AstroMap({ astroType, selectedPlanetId, speedRatio }: AstroMapProps) {
+function AstroMap({ planets, selectedPlanetId, speedRatio }: AstroMapProps) {
     const [data, setData] = useState<Astre[]>([]);
     const [soleil, setSoleil] = useState<Astre | undefined>(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
 
-    useEffect(() => {
-        console.log(selectedPlanetId);
-    }, [selectedPlanetId]);
-
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const results = await Promise.all(
-                    astroType.map(planet => getPlanetById(planet.id))
-                );
-                setData(results);
-                setSoleil(results.find(planet => planet.id === 'soleil'));
-            } catch (error) {
-                setError(error as Error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [astroType]);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
@@ -57,36 +34,36 @@ function AstroMap({ astroType, selectedPlanetId, speedRatio }: AstroMapProps) {
             <pointLight distance={0} decay={0.01} intensity={5} />
 
 
-            {data.filter(astroBody => astroBody.id).map(astroBody => (
-                <React.Fragment key={astroBody.id}>
+            {data.filter(planets => planets.id).map(planet => (
+                <React.Fragment key={planet.id}>
                     <OrbitLine
-                        semiMajorAxis={scaleOrbit(astroBody.semimajorAxis)}
+                        semiMajorAxis={scaleOrbit(planet.semimajorAxis)}
                         orbitCenter={new Vector3(0, 0, 0)}
                         lineOpacity={(selectedPlanetId === "soleil") ? 0.3 : 0}
                     />
                     <AstroPlanet
-                        key={astroBody.id}
-                        name={astroBody.id}
-                        radius={scaleRadius(astroBody.equaRadius)}
+                        key={planet.id}
+                        name={planet.id}
+                        radius={scaleRadius(planet.equaRadius)}
                         widthSegments={128}
                         heightSegments={64}
-                        texture={`2k_${astroBody.id}.jpg`}
-                        sideralOrbit={scaleSideralOrbit(astroBody.sideralOrbit) * speedRatio}
-                        distance={scaleOrbit(astroBody.semimajorAxis)}
-                        rotationSpeed={scaleSideralRotation(astroBody.sideralRotation) * speedRatio}
-                        axialTilt={astroBody.axialTilt}
+                        texture={`2k_${planet.id}.jpg`}
+                        sideralOrbit={scaleSideralOrbit(planet.sideralOrbit) * speedRatio}
+                        distance={scaleOrbit(planet.semimajorAxis)}
+                        rotationSpeed={scaleSideralRotation(planet.sideralRotation) * speedRatio}
+                        axialTilt={planet.axialTilt}
                         speedMultiplier={100}
                         timeDilation={100}
-                        hasRing={(astroBody.id == "saturne") ? true : false}
-                        ringTexture={(astroBody.id == "saturne") ? `2k_${astroBody.id}_ring.png` : undefined}
+                        hasRing={(planet.id == "saturne") ? true : false}
+                        ringTexture={(planet.id == "saturne") ? `2k_${planet.id}_ring.png` : undefined}
                     />
                 </React.Fragment>
             ))}
             <OrbitControls />
             <Stars />
             <CustomCamera 
-                cameraPositionOffset={astroType.find(planet => planet.id === selectedPlanetId)?.cameraPositionOffset} 
-                cameraLookAtOffset={astroType.find(planet => planet.id === selectedPlanetId)?.cameraLookAtOffset} 
+                cameraPositionOffset={planets.find(planet => planet.id === selectedPlanetId)?.cameraPositionOffset} 
+                cameraLookAtOffset={planets.find(planet => planet.id === selectedPlanetId)?.cameraLookAtOffset} 
                 cameraPlanetFocused={selectedPlanetId} 
             />
         </Canvas>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AstroHeader from "./components/AstroHeader";
 import AstroNav from "./components/AstroNav";
 import "./variables.css";
 import AstroMap from "./components/AstroMap";
 import AstreDetails from "./components/AstreDetails";
 import { Box, Slider, Typography } from "@mui/material";
+import getPlanetById from "./services/ApiService";
+import { Astre } from "./types/bodies";
 
 export interface AstroType {
   id: string;
@@ -14,62 +16,98 @@ export interface AstroType {
   cameraLookAtOffset: number;
 }
 
+const astroPlanetsToDisplay: AstroType[] = [
+  {
+    id: "soleil",
+    cameraPositionOffset: 1500,
+    cameraLookAtOffset: -2.4,
+  },
+  {
+    id: "mercure",
+    cameraPositionOffset: 6,
+    cameraLookAtOffset: -2.4,
+  },
+  {
+    id: "venus",
+    cameraPositionOffset: 14,
+    cameraLookAtOffset: -5.4,
+  },
+  {
+    id: "terre",
+    cameraPositionOffset: 14,
+    cameraLookAtOffset: -5.4,
+  },
+  {
+    id: "mars",
+    cameraPositionOffset: 8,
+    cameraLookAtOffset: -4,
+  },
+  {
+    id: "jupiter",
+    cameraPositionOffset: 140,
+    cameraLookAtOffset: -40,
+  },
+  {
+    id: "saturne",
+    cameraPositionOffset: 250,
+    cameraLookAtOffset: -120,
+  },
+  {
+    id: "neptune",
+    cameraPositionOffset: 60,
+    cameraLookAtOffset: -30,
+  },
+  {
+    id: "uranus",
+    cameraPositionOffset: 60,
+    cameraLookAtOffset: -20,
+  },
+];
+
 function Home() {
   const [sliderValue, setSliderValue] = useState(50); // 50 corresponds to the center value 1 in the new scale
   const [speedRatio, setSpeedRatio] = useState(1);
+  const [data, setData] = useState<Astre[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  
 
-  const astroPlanets: AstroType[] = [
-    {
-      id: "soleil",
-      cameraPositionOffset: 1500,
-      cameraLookAtOffset: -2.4,
-    },
-    {
-      id: "mercure",
-      cameraPositionOffset: 6,
-      cameraLookAtOffset: -2.4,
-    },
-    {
-      id: "venus",
-      cameraPositionOffset: 14,
-      cameraLookAtOffset: -5.4,
-    },
-    {
-      id: "terre",
-      cameraPositionOffset: 14,
-      cameraLookAtOffset: -5.4,
-    },
-    {
-      id: "mars",
-      cameraPositionOffset: 8,
-      cameraLookAtOffset: -4,
-    },
-    {
-      id: "jupiter",
-      cameraPositionOffset: 140,
-      cameraLookAtOffset: -40,
-    },
-    {
-      id: "saturne",
-      cameraPositionOffset: 250,
-      cameraLookAtOffset: -120,
-    },
-    {
-      id: "neptune",
-      cameraPositionOffset: 60,
-      cameraLookAtOffset: -30,
-    },
-    {
-      id: "uranus",
-      cameraPositionOffset: 60,
-      cameraLookAtOffset: -20,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const results = await Promise.all(
+          astroPlanetsToDisplay.map((planet) => getPlanetById(planet.id))
+        );
 
-  const [selectedPlanet, setSelectedPlanet] = useState<AstroType>(astroPlanets[0]);
+        // Combine the API data with the local offsets
+        const combinedData = results.map((apiData) => {
+          const offsets = astroPlanetsToDisplay.find(
+            (planet) => planet.id === apiData.id
+          );
+          return {
+            ...apiData,
+            ...offsets,
+          };
+        });
+
+        setData(combinedData);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+ 
+
+  const [selectedPlanet, setSelectedPlanet] = useState<AstroType>(astroPlanetsToDisplay[0]);
+
 
   const handlePlanetChange = (newPlanet: string) => {
-    const planet = astroPlanets.find((p) => p.id === newPlanet);
+    const planet = astroPlanetsToDisplay.find((p) => p.id === newPlanet);
     if (planet) setSelectedPlanet(planet);
   };
 
@@ -100,18 +138,22 @@ function Home() {
         />
       </Box>
       <AstroHeader />
-      <AstroMap
+      {loading ? <div>Loading...</div> : error ? <div>Error: {error.message}</div> : (
+      <div>
+        <AstroMap
         speedRatio={speedRatio}
-        astroType={astroPlanets}
+        planets={data}
         selectedPlanetId={selectedPlanet.id}
       />
       <AstreDetails id={selectedPlanet.id} isVisible={false} />
       <AstroNav
-        planets={astroPlanets.map((p) => p.id)}
+        planets={astroPlanetsToDisplay.map((p) => p.id)}
         onPlanetChange={handlePlanetChange}
       />
+      </div>
+      )}
+ 
     </div>
   );
 }
-
 export default Home;

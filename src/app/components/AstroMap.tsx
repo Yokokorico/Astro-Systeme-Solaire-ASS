059422cf8008278894da14c3ee9c67352852eb9c
Théo Duflos <152173306+Yokokorico@users.noreply.github.com>
2024-08-08 09:21:11 +0200

    feat: :sparkles: add background to the scene

--- src/app/components/AstroMap.tsx
+++ src/app/components/AstroMap.tsx
@@ -1,61 +1,93 @@
-import React from 'react';
-import { Astre } from '../types/bodies';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { scaleOrbit, scaleRadius, scaleSideralOrbit, scaleSideralRotation } from '../utils/Conversion';
-import { CustomCamera } from './CustomCamera';
-import { OrbitLine } from './OrbitLine';
-import { Vector3 } from 'three';
-import AstroPlanet from './AstroPlanet';
-import Stars from './Stars';
+import React, { useRef, useEffect } from "react";
+import { Astre } from "../types/bodies";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import {
+  scaleOrbit,
+  scaleRadius,
+  scaleSideralOrbit,
+  scaleSideralRotation,
+} from "../utils/Conversion";
+import { CustomCamera } from "./CustomCamera";
+import { OrbitLine } from "./OrbitLine";
+import { Vector3 } from "three";
+import * as THREE from "three";
+import AstroPlanet from "./AstroPlanet";
+import Stars from "./Stars";
 
 export interface AstroMapProps {
-    planets: Astre[];
-    selectedPlanetId: string;
-    speedRatio: number;
+  planets: Astre[];
+  selectedPlanetId: string;
+  speedRatio: number;
+}
+
+function SetEnvironment() {
+  const { scene } = useThree();
+  const envMap = useTexture("/gaia.png");
+  envMap.mapping = THREE.EquirectangularReflectionMapping;
+
+  useEffect(() => {
+    scene.background = envMap;
+  }, [scene, envMap]);
+
+  return null;
 }
 
 function AstroMap({ planets, selectedPlanetId, speedRatio }: AstroMapProps) {
-    if (!planets.length) return <div>AstroMapLoading...</div>;
+  const canvasRef = useRef(null);
 
-    return (
-        <Canvas style={{ width: '100vw', height: '100vh' }}>
-            <ambientLight intensity={0.2} />
-            <pointLight distance={0} decay={0.01} intensity={5} />
+  if (!planets.length) return <div>AstroMapLoading...</div>;
 
-            {planets.filter(planet => planet.id).map(planet => (
-                <React.Fragment key={planet.id}>
-                    <OrbitLine
-                        semiMajorAxis={scaleOrbit(planet.semimajorAxis)}
-                        orbitCenter={new Vector3(0, 0, 0)}
-                        lineOpacity={(selectedPlanetId === "soleil") ? 0.3 : 0}
-                    />
-                    <AstroPlanet
-                        name={planet.id}
-                        radius={scaleRadius(planet.equaRadius)}
-                        widthSegments={128}
-                        heightSegments={64}
-                        texture={`2k_${planet.id}.jpg`}
-                        sideralOrbit={scaleSideralOrbit(planet.sideralOrbit) * speedRatio}
-                        distance={scaleOrbit(planet.semimajorAxis)}
-                        rotationSpeed={scaleSideralRotation(planet.sideralRotation) * speedRatio}
-                        axialTilt={planet.axialTilt}
-                        hasRing={planet.id === "saturne"}
-                        ringTexture={planet.id === "saturne" ? `2k_${planet.id}_ring.png` : undefined}
-                        speedMultiplier={100}
-                        timeDilation={100}
-                    />
-                </React.Fragment>
-            ))}
-            <OrbitControls />
-            {/* <Stars /> */}
-            <CustomCamera 
-                cameraPositionOffset={planets.find(planet => planet.id === selectedPlanetId)?.cameraPositionOffset || 0} 
-                cameraLookAtOffset={planets.find(planet => planet.id === selectedPlanetId)?.cameraLookAtOffset || 0} 
-                cameraPlanetFocused={selectedPlanetId} 
+  return (
+    <Canvas ref={canvasRef} style={{ width: "100vw", height: "100vh" }}>
+      <ambientLight intensity={0.2} />
+      <pointLight distance={0} decay={0.01} intensity={5} />
+      <SetEnvironment />
+      {planets
+        .filter((planet) => planet.id)
+        .map((planet) => (
+          <React.Fragment key={planet.id}>
+            <OrbitLine
+              semiMajorAxis={scaleOrbit(planet.semimajorAxis)}
+              orbitCenter={new Vector3(0, 0, 0)}
+              lineOpacity={selectedPlanetId === "soleil" ? 0.3 : 0}
             />
-        </Canvas>
-    );
+            <AstroPlanet
+              name={planet.id}
+              radius={scaleRadius(planet.equaRadius)}
+              widthSegments={128}
+              heightSegments={64}
+              texture={`2k_${planet.id}.jpg`}
+              sideralOrbit={scaleSideralOrbit(planet.sideralOrbit) * speedRatio}
+              distance={scaleOrbit(planet.semimajorAxis)}
+              rotationSpeed={
+                scaleSideralRotation(planet.sideralRotation) * speedRatio
+              }
+              axialTilt={planet.axialTilt}
+              hasRing={planet.id === "saturne"}
+              ringTexture={
+                planet.id === "saturne" ? `2k_${planet.id}_ring.png` : undefined
+              }
+              speedMultiplier={100}
+              timeDilation={100}
+            />
+          </React.Fragment>
+        ))}
+      <OrbitControls />
+      {/* <Stars /> */}
+      <CustomCamera
+        cameraPositionOffset={
+          planets.find((planet) => planet.id === selectedPlanetId)
+            ?.cameraPositionOffset || 0
+        }
+        cameraLookAtOffset={
+          planets.find((planet) => planet.id === selectedPlanetId)
+            ?.cameraLookAtOffset || 0
+        }
+        cameraPlanetFocused={selectedPlanetId}
+      />
+    </Canvas>
+  );
 }
 
 export default AstroMap;

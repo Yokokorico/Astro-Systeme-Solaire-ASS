7f607647d2e61aacac9c9585e7e6427d49cf6f64
Merlinovitch <a.merlin92@gmail.com>
2024-08-27 14:23:58 +0200

    feat: :sparkles: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,38 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Astronomical Solar System",
-  description: "Astronomical Solar System",
+  description: "Le système solaire en 3D avec ThreeJS",
+  viewport: "width=device-width, initial-scale=1",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  keywords: [
+    "système solaire",
+    "ThreeJS",
+    "3D",
+    "astronomie",
+    "planètes",
+    "réalité virtuelle",
+  ],
+  openGraph: {
+    type: "website",
+    locale: "fr_FR",
+    url: "https://astro-systeme-solaire-ass.vercel.app/", 
+    title: "Astronomical Solar System",
+    description: "Découvrez le système solaire en 3D avec ThreeJS",
+    images: [
+      {
+        url: "/assets/images/description-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "Astronomical Solar System",
+      },
+    ],
+  },
 };
+
+
 
 export default function RootLayout({
   children,
@@ -15,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>{children}</body>
     </html>
   );
